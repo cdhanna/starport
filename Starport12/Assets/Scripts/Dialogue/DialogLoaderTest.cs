be5611be8c5649e240773174ac84fb50eb2516cf
Will <usparatrooper52@gmail.com>
2018-02-18 17:29:24 -0500

    initial commit to branch

---
+++ Starport12/Assets/Scripts/Dialogue/DialogLoaderTest.cs
@@ -0,0 +1,20 @@
+﻿using Dialog;
+using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class DialogLoaderTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var json = File.ReadAllText("Assets\\sample json data\\sampleDialog1.json");
+        var rules = JsonConvert.DeserializeObject<DialogRule[]>(json);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Starport12/Assets/TextController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour {
+
+    public Text text;
+
+	// Use this for initialization
+	void Start () {
+        string txt = System.IO.File.ReadAllText(@"C:\Users\Will\Proj\starport\wh_texts\dummy.txt");
+        text.text = txt;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
